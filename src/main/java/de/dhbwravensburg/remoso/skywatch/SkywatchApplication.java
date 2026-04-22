7c2e36640064d961f12699bfe9b700e81bcfcaee
Katrin Schaake <katrin.schaake@remoso.com>
2026-04-22 09:21:10 +0200

    Add Spring Boot project skeleton

---
+++ src/main/java/de/dhbwravensburg/remoso/skywatch/SkywatchApplication.java
@@ -0,0 +1,13 @@
+package de.dhbwravensburg.remoso.skywatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkywatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkywatchApplication.class, args);
+	}
+
+}
---
+++ src/main/java/de/dhbwravensburg/remoso/skywatch/controller/HealthController.java
@@ -0,0 +1,21 @@
+package de.dhbwravensburg.remoso.skywatch.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+	@GetMapping("/api/health")
+	public Map<String, String> health() {
+		return Map.of(
+				"status", "UP",
+				"application", "SkyWatch",
+				"version", "0.1.0"
+		);
+	}
+
+}
